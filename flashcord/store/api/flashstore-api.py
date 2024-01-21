4844398ec1db7de1f47b95607040908486affc08
Sirius B <root.siriusb@gmail.com>
2024-01-21 22:56:19 +0100

    New api looking fire (still has bugs though)

--- flashcord/store/api/flashstore-api.py
+++ flashcord/store/api/flashstore-api.py
@@ -8,32 +8,113 @@ def FlashStore_API():
     ServerPort = 1407
     PacketSize = 1024
     API_Socket = socket.socket()
-    DebugMode = True
     API_Version = "2"
+    DebugMode = True
+    MaxTimeoutCount = 10
+    MaxIdleCount = 1000000
+    RetryDelay = 1
 
-    def API_Deliverer(RemoteClient):
+    def FillRequest(RemoteClient, RequestData, ClientAddress):
+        Sending(RemoteClient)
+        TimeoutCount = 0
+        while True:
+            RemoteClient_Response = RemoteClient.recv(PacketSize).decode()
+            if TimeoutCount == MaxTimeoutCount:
+                print(f'[Flashstore API // Server] {ClientAddress} timed out.')
+                break
+            elif RemoteClient_Response == "READY":
+                try:
+                    print(f'[Flashstore API // Server] Sending to {ClientAddress} the data "{RequestData}".')
+                    RemoteClient.send(str.encode(RequestData))
+                    Done(RemoteClient)
+                    break
+                except:
+                    if DebugMode == True: print(f"TIMEOUT: {TimeoutCount}/{MaxTimeoutCount} Couldn't get new data from {ClientAddress}")
+                    TimeoutCount+=1
+                    time.sleep(RetryDelay)
+            else:
+                if DebugMode == True: print(f"TIMEOUT: {TimeoutCount}/{MaxTimeoutCount} Couldn't get new data from {ClientAddress}")
+                TimeoutCount+=1
+                time.sleep(RetryDelay)
+                
+    def isPreciseRequest(API_Request):
+        isPreciseRequest = True
+        try:
+            DummyVar = API_Request[2]
+        except:
+            isPreciseRequest = False
+        return isPreciseRequest
+
+    def API_Deliverer(RemoteClient, ClientAddress):
         RemoteClient_Response = RemoteClient.recv(PacketSize).decode()
         if RemoteClient_Response != API_Version:
-            print(f'[Flashstore API // Server] Requested API Version: {RemoteClient_Response}. [INVALID_API-VERSION]')
+            print(f'[Flashstore API // Server] {ClientAddress} Requested API Version: {RemoteClient_Response}. [INVALID_API-VERSION]')
             InvalidVersion(RemoteClient)
             RemoteClient.close()
         else:
-            print(f'[Flashstore API // Server] Requested API Version: {RemoteClient_Response}. [OK]')
+            print(f'[Flashstore API // Server] {ClientAddress} Requested API Version: {RemoteClient_Response}. [OK]')
             OK(RemoteClient)
+        TimeoutCount = 0
+        IdleCount = 0
         while True:
-            RemoteClient_Response = RemoteClient.recv(PacketSize).decode()
-            if RemoteClient_Response == "":
-                if DebugMode == True: print("Client didn't send new data.")
+            try:
+                RemoteClient_Response = RemoteClient.recv(PacketSize).decode()
+            except:
+                if DebugMode == True: print(f"TIMEOUT: {TimeoutCount}/{MaxTimeoutCount} Couldn't get new data from {ClientAddress}")
+                TimeoutCount+=1
+                time.sleep(RetryDelay)
+
+            if TimeoutCount == MaxTimeoutCount:
+                print(f'[Flashstore API // Server] {ClientAddress} timed out.')
+                break
+            elif IdleCount == MaxIdleCount:
+                print(f'[Flashstore API // Server] {ClientAddress} idled for too long..')
+                break
             else:
-                print(f'[Flashstore API // Server] Received Code: {RemoteClient_Response}.')
-        print(f'[Flashstore API // Server] Reached end of loop.')
+                if RemoteClient_Response == "":
+                    if DebugMode == True: print(f"IDLE: {IdleCount}/{MaxIdleCount} Didn't get new data from {ClientAddress}")
+                    IdleCount+=1
+                    time.sleep(RetryDelay)
+                else:
+                    print(f'[Flashstore API // Server] Received Request: "{RemoteClient_Response}".')
+                    API_Request = RemoteClient_Response.replace("/"," ").split()
+                    if API_Request[0] == "GET":
+                            if API_Request[1] == "MODULES":  
+                                if isPreciseRequest(API_Request) == True:
+                                    if API_Request[2].lower() in PluginData:
+                                        FillRequest(RemoteClient, ModuleData[ModuleData.index(API_Request[2].lower()) + 1], ClientAddress)
+                                    else:
+                                        NotFound(RemoteClient)
+                                else:
+                                    FillRequest(RemoteClient, ModuleData, ClientAddress)
+                                    Done(RemoteClient)
+
+                            elif API_Request[1] == "PLUGINS":
+                                if isPreciseRequest(API_Request) == True:
+                                    if API_Request[2].lower() in PluginData:
+                                        FillRequest(RemoteClient, PluginData[PluginData.index(API_Request[2].lower()) + 1], ClientAddress)
+                                    else:
+                                        NotFound(RemoteClient)
+                                else:
+                                    FillRequest(RemoteClient, PluginData, ClientAddress)
+                                    
+                            elif API_Request[1] == "USERS":
+                                FillRequest(RemoteClient, UserData, ClientAddress)
+                            else:
+                                NotFound(RemoteClient)
+                    else:
+                        NotFound(RemoteClient)
+        if DebugMode == True: print(f'Reached end of loop.')
         RemoteClient.close()
 
     def IncomingConnection(Connection_Socket):
         Client, Address = Connection_Socket.accept()
         print(f'[Flashstore API] NOTICE: Incoming connection from {Address[0]}:{Address[1]}.')
-        ClientAddress = str(Address[0])+str(Address[1])
-        _thread.start_new_thread(API_Deliverer, (Client, ))
+        ClientAddress = str(Address[0])+":"+str(Address[1])
+        try:
+            _thread.start_new_thread(API_Deliverer, (Client,ClientAddress))
+        except:
+            print(f'[Flashstore API // Server] Giving up {ClientAddress}.')
 
     # Status Codes
     def InvalidVersion(RemoteClient):
@@ -42,6 +123,17 @@ def FlashStore_API():
     
     def OK(RemoteClient):
         RemoteClient.send(str.encode('OK'))
+
+    def NotFound(RemoteClient):
+        RemoteClient.send(str.encode('NOT_FOUND'))
+        RemoteClient.close()
+
+    def Done(RemoteClient):
+        RemoteClient.send(str.encode('DONE'))
+        RemoteClient.close()
+
+    def Sending(RemoteClient):
+        RemoteClient.send(str.encode('SENDING'))
 
     print(f'[Flashstore API] INFO: Initializing Server...\n')
     
--- flashcord/store/api/flashstore-client.py
+++ flashcord/store/api/flashstore-client.py
@@ -1,7 +1,10 @@
 import socket
 import time 
 
-def FlashClient_API_Request():
+def FlashClient_API_Request(API_Request):
+    def Ready(RemoteServer):
+        print(f'[Flashstore API // Client] Sent to the server that we are ready to receive data.')
+        RemoteServer.send(str.encode('READY'))
 
     print(f'[Flashstore API // Client] INFO: Attempting to communicate with the FlashStore API...')
     # Server Information
@@ -9,8 +12,9 @@ def FlashClient_API_Request():
     ServerPort = 1407
     PacketSize = 1024
     RemoteServer = socket.socket()
-    DebugMode = False
     API_Version = "2"
+    DebugMode = False
+    Data = []
     try:
         RemoteServer.connect((ServerAddress, ServerPort))
     except socket.error as ErrorInfo:
@@ -18,11 +22,35 @@ def FlashClient_API_Request():
     print(f'[Flashstore API // Client] Successfully connected to {ServerAddress}:{ServerPort}.')
     print(f'[Flashstore API // Client] Sent that we are requesting to use version v{API_Version} of the API.')
     RemoteServer.send(str.encode(API_Version))
-    while True:
-        RemoteServer_Response = RemoteServer.recv(PacketSize).decode()
-        if RemoteServer_Response == "":
-            if DebugMode == True: print("Server didn't send new data.")
-        else:
-            print(f'[Flashstore API // Client] Received Code: {RemoteServer_Response}.')
+    RemoteServer_Response = RemoteServer.recv(PacketSize).decode()
+    if True and RemoteServer_Response == "OK":
+        print(f'[Flashstore API // Client] Received Code: {RemoteServer_Response}, starting loop.')
+        print(f'[Flashstore API // Client] Requesting to the server "{API_Request}".')
+        RemoteServer.send(str.encode(API_Request))
+        while True:
+            RemoteServer_Response = RemoteServer.recv(PacketSize).decode()
+            if RemoteServer_Response == "":
+                if DebugMode == True: print("Server didn't send new data.")
+            else:
+                print(f'[Flashstore API // Client] Received Code: {RemoteServer_Response}.')
+                if RemoteServer_Response == "DONE":
+                    return Data
+                elif RemoteServer_Response == "SENDING":
+                    Ready(RemoteServer)
+                    while RemoteServer_Response != "DONE":
+                        RemoteServer_Response = RemoteServer.recv(PacketSize).decode()
+                        if RemoteServer_Response == "":
+                            if DebugMode == True: print("Server didn't send new data.")
+                            time.sleep(1)
+                        elif RemoteServer_Response != "DONE":
+                            print(f'[Flashstore API // Client] Server filled our request with the data "{RemoteServer_Response}".')
+                            Data = RemoteServer_Response
+                    return Data
+    else:
+        print(f'[Flashstore API // Client] The Flashstore Client is outdated! Server sent "{RemoteServer_Response}".')
 
-FlashClient_API_Request()
+print('\n\n===================\nRetrieved the data:', FlashClient_API_Request("GET/MODULES/SIRIUSBYT"), '\n===================\n\n') # Works
+print('\n\n===================\nRetrieved the data:', FlashClient_API_Request("GET/PLUGINS/THARKI-GOD"), '\n===================\n\n') # Works
+#print('\n\n===================\nRetrieved the data:', FlashClient_API_Request("GET/USERS"), '\n===================\n\n') # Hangs the Client
+#print('\n\n===================\nRetrieved the data:', FlashClient_API_Request("GET/MODULES"), '\n===================\n\n') # Also hangs the Client
+#print('\n\n===================\nRetrieved the data:', FlashClient_API_Request("GET/PLUGINS"), '\n===================\n\n') # Still hangs the Client
