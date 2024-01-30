1803e1afa9187b5042b48b8ce85118740a1b5aab
Sirius B <root.siriusb@gmail.com>
2024-01-30 23:09:15 +0100

    lel

--- flashcord/store/api/Flashstore_API-Server-RW.py
+++ flashcord/store/api/Flashstore_API-Server-RW.py
@@ -1,18 +1,26 @@
+# Server Modules
 import socket # API's Native Language 
 import websockets # API's Translated Language
-import time
-import asyncio
-import _thread
-import threading
-import json
-import os
-import sys
+
+# Multi-threading modules
+import asyncio # Required for WebSockets
+import _thread # Required for multi-client raw socket clients.
+import threading # Required for WebSockets
+
+# Miscellaneous Modules
+import time # Required for logs
+import json # Server Data is in JSON format
+import os # Required for KeyboardInterrupt (DEBUG)
+import sys # Required for KeyboardInterrupt (DEBUG)
+
+# Flashstore Scripts
 from Flashstore_RefreshJSON import *
 
 """
-Very important almighty Flashstore API Banner
-Legends say that removing this banner will cause the world to explode.
+EXTERNAL DEPENDENCIES TO INSTALL:
+- WebSockets
 """
+
 ASCII_Banner = " ▄▄▄▄▄▄▄ ▄▄▄     ▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   ▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄ \n\
 █       █   █   █      █       █  █ █  █       █       █       █   ▄  █ █       █  █       █       █   █ \n\
 █    ▄▄▄█   █   █  ▄   █  ▄▄▄▄▄█  █▄█  █  ▄▄▄▄▄█▄     ▄█   ▄   █  █ █ █ █    ▄▄▄█  █   ▄   █    ▄  █   █ \n\
@@ -21,6 +29,11 @@ ASCII_Banner = " ▄▄▄▄▄▄▄ ▄▄▄     ▄▄▄▄▄▄ ▄▄�
 █   █   █       █  ▄   █▄▄▄▄▄█ █  █ █  █▄▄▄▄▄█ █ █   █ █       █   █  █ █   █▄▄▄   █   ▄   █   █   █   █ \n\
 █▄▄▄█   █▄▄▄▄▄▄▄█▄█ █▄▄█▄▄▄▄▄▄▄█▄▄█ █▄▄█▄▄▄▄▄▄▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄█  █▄█▄▄▄▄▄▄▄█  █▄▄█ █▄▄█▄▄▄█   █▄▄▄█ \
 "
+"""
+Very important almighty Flashstore API Banner
+Legends say that removing this banner will cause the world to explode.
+"""
+
 # Server Information
 Server_Address = socket.gethostname()
 Server_Port = 1407
@@ -32,12 +45,9 @@ DebugMode = False
 
 # This is useless and is only used once but hey, you never know if you gonna need this useless piece of shit. I swear I'm not a function hoarder.
 def Flood(Lines):
-    Flood = ""
-    for cycle in range (Lines+1):
-        Flood = Flood + "\n"
-    return Flood
+    for cycle in range (Lines+1): print("\n")
 
-# Logging System + Getting current time and date in prefered format
+# Logging System + Getting current time and date in preferred format
 def GetTime():
     CTime = time.localtime()
     Time = f"{CTime.tm_hour:02d}:{CTime.tm_min:02d}:{CTime.tm_sec:02d}"
@@ -53,7 +63,13 @@ def WriteLog(Log,isDebugMessage):
         if isDebugMessage == True and DebugMode == True: print(PrintLog)
         elif isDebugMessage == False: print(PrintLog)
 
-# Server Data Loader
+# Server Data Loader & Debug Display
+def Display_Data():
+    WriteLog(f'SYSTEM: Server Data: {Data_Server}',True)
+    WriteLog(f'SYSTEM: Module Data: {Data_Modules}',False)
+    WriteLog(f'SYSTEM: Plugin Data: {Data_Plugins}',False)
+    WriteLog(f'SYSTEM: Theme Data: {Data_Themes}',False)
+    WriteLog(f'SYSTEM: User Data: {Data_Users}',False)
 def GetServerData():
     global Data_Modules,Data_Plugins,Data_Themes,Data_Users,Data_Server
     WriteLog(f'SYSTEM: Refreshing JSON...',False)
@@ -68,12 +84,6 @@ def GetServerData():
     Display_Data()
     return Data_Modules,Data_Plugins,Data_Themes,Data_Users,Data_Server
 
-def Display_Data():
-    WriteLog(f'SYSTEM: Server Data: {Data_Server}',True)
-    WriteLog(f'SYSTEM: Module Data: {Data_Modules}',False)
-    WriteLog(f'SYSTEM: Plugin Data: {Data_Plugins}',False)
-    WriteLog(f'SYSTEM: Theme Data: {Data_Themes}',False)
-    WriteLog(f'SYSTEM: User Data: {Data_Users}',False)
 
 
 # Server Handlers
@@ -83,13 +93,12 @@ def WebSocket_Server():
         Client_Address = str(Address[0])+":"+str(Address[1])
         WriteLog(f'SUCCESS: New WebSocket connection from "{Client_Address}".',False)
         await Application_Programming_Interface(websocket,Client_Address,True)
-
     async def Websocket_Listener():
         WriteLog(f'INFO: Initiated WebSocket Translation Layer.',False)
         async with websockets.serve(Websocket_Handler, "localhost", 1407):
             await asyncio.Future()
-
     asyncio.run(Websocket_Listener())
+
 def Socket_Server():
     def Socket_Handler(Connection_Socket):
         Address = Connection_Socket.accept()
@@ -97,29 +106,21 @@ def Socket_Server():
         WriteLog(f"CONNECTION: {Client_Address} [SUCCESS]",True)
         try: _thread.start_new_thread(Application_Programming_Interface, (Connection_Socket, Client_Address, False))
         except Exception as ErrorInfo: WriteLog(f'DROPPED: {Client_Address}. \n[TRACEBACK]\n{ErrorInfo}\n',True)
-
     try: API_Socket.bind((Server_Address, Server_Port))
     except socket.error as ErrorInfo: WriteLog(f"CRITICAL ERROR: Failed to bind the server's address! \n[TRACEBACK]\n{ErrorInfo}\n",False)
     WriteLog(f'SYSTEM: Initiated Socket Server.',False)
     API_Socket.listen()
-
-    while True:
-        Socket_Handler(API_Socket)
+    while True: Socket_Handler(API_Socket)
 
 # WARNING: Async hell. Why? Because WebSockets are fucking ass and complain & errors out if you don't await the .recv()
 async def Application_Programming_Interface(Client,Client_Address,isWebSocket):
     # Key Handling Functions
-    async def Close_Connection():
-        WriteLog(f"CLOSING: {Client_Address}.", False)
-        await Client.close()
+    async def Close_Connection(): WriteLog(f"CLOSING: {Client_Address}.", False); await Client.close()
     async def ReceiveData():
         try:
             if isWebSocket == True: Response = await Client.recv()
             else: Response = await Client.recv(Packet_Size).decode()
-        except:
-            WriteLog(f"TIMED OUT: {Client_Address}.", False)
-            await Close_Connection()
-            return
+        except: WriteLog(f"TIMED OUT: {Client_Address}.", False); await Close_Connection(); return
         return Response
     async def Send(Packet):
         if isWebSocket == True: await Client.send(Packet)
@@ -138,9 +139,7 @@ async def Application_Programming_Interface(Client,Client_Address,isWebSocket):
     async def CheckVersion():
         Client_Data = await ReceiveData()
         try: Client_Version = float(Client_Data)
-        except:
-            WriteLog(f'API Version Check // ERROR: {Client_Address} Invalid "{Client_Data}" API Version!', False)
-            await Code_Client_Invalid_Version(); return
+        except: WriteLog(f'API Version Check // ERROR: {Client_Address} Invalid "{Client_Data}" API Version!', False); await Code_Client_Invalid_Version(); return
         if Client_Version == Server_API_Version: WriteLog(f'API Version Check // OK: {Client_Address}@API_"{Client_Data}"', False); await Code_OK(); return True
         elif Client_Version > Server_API_Version: WriteLog(f'API Version Check // WARNING: {Client_Address}@API_"{Client_Data}" is newer.', False); await Code_Server_Outdated(); return True
         elif Client_Version < Server_API_Version: WriteLog(f'API Version Check // ERROR: {Client_Address}@API_"{Client_Data}" is outdated!', False); await Code_Client_Outdated(); return False
@@ -159,11 +158,46 @@ async def Application_Programming_Interface(Client,Client_Address,isWebSocket):
             return
     else: return
 
-
-def Bootstrap():
-    print(Flood(16))
+# Glorious.
+def SplashBanner():
+    Flood(16)
     print(ASCII_Banner)
     print(f"{Server_Address}:{Server_Port}@API_{Server_API_Version}/{Server_Version} // Debug: {DebugMode} - Packet Size: {Packet_Size}b\n")
+
+"""
+
+- "Hey Sirius, what happens when you start the Flashstore API?"
+
+...
+
+It starts with
+one thing
+I don't know why,
+it doesn't even matter how hard you try
+keep that in mind I designed this rhyme to explain in due time that [all I know]
+Time is a valuable thing,
+watch it fly by as the pendulum swings,
+the clock ticks life away,
+it's so unreal.
+Didn't look out below,
+watch the time go out the window,
+didn't even know,
+I wasted all that time just to [watch you [go]]
+Even though I tried,
+it all fell apart.
+What it eventually means to me,
+will be a memory
+of a time [I tried so hard.]
+And got so far,
+but in the end,
+it doesn't even matter...
+
+Okay I'm going insane. That's all I remember of the song, go fuck yourself.
+Go add the rest of the lyrics yourself.
+
+"""
+def Bootstrap():
+    SplashBanner()
     WriteLog(f'SYSTEM: Initializing server...',False)
 
     # Retrieve server data
@@ -175,8 +209,11 @@ def Bootstrap():
         # Server Routine after Server Execution
         while True: 
             time.sleep(1)
+            # There would be more code here, but right now I'm lazy.
     except KeyboardInterrupt:
         try: sys.exit(130)
         except SystemExit: os._exit(130)
 
-Bootstrap()
+# Spark
+if __name__== '__main__': Bootstrap()
+else: print("The fuck you doing m8? This isn't a module!")
